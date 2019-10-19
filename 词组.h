cc5627eb38d7d20833aec79d61e9c6e0a736313c
liqingyu-la <55045352+liqingyu-la@users.noreply.github.com>
2019-10-19 20:04:29 +0800

    Add files via upload

--- 词组.h
+++ 词组.h
@@ -7,36 +7,33 @@ using namespace std;
 	int flag;//0为非单词 
 	int flag1;//判断是否遍历过
 	bool ifend;//是否在文末 
-};
-*/
+};*/
+
 struct ph{
 	string cizu;
 	int num;
 	int f;
 };
 
-struct ph phrase(struct str *a,int m){
+struct ph phrase(struct str *a,int m,int q){//a为结构体数组、m为词组包含词数、q为词个数 
 	struct ph b[500],d[500];
 	int n=0;
 	//string cz[100]; 
-	//printf("%d ",sizeof(a[0]));
+	//printf("%d ",sizeof(*a));
 	//printf("%d ",sizeof(str));
 //	printf("#####%d\n",a[0].ifend );
-	int len=sizeof(a)/sizeof(struct str);
+	int len=sizeof(*a)/sizeof(struct str);
 
 	string c ="";//临时存放字符串 
-	for(int i=0; i<len; i++){
+	for(int i=0; i<q; i++){
 
 		for(int l=0;l<m;l++){
 			//printf("%d ",a[i].flag);
 			//printf("%s",a[i].ifend==false?"FALSE":"TRUE");
-			if((a[i+l].flag==0) || a[i+l-1].ifend==true){
-
+			if((a[i+l].flag==0) ||((a[i+l].ifend==true) && l!=m-1)){
 				//printf("%d 跳过\n",i);
 				break;
-			}//非单词直接break，移到下一位 
-			//if()
-			//在m内遇见行末单词，直接break，移到下一位 
+			}//非单词直接break，移到下一位、 在m内遇见行末单词，直接break，移到下一位 
 			c =  c+a[i+l].name+' ';//可以直接加空格吗 
 			//printf("%s ",a[i].name.c_str());
 			//printf("%d ",a[i].ifend );
@@ -59,28 +56,35 @@ struct ph phrase(struct str *a,int m){
 			
 			continue;//结构体为空下移一个元素 
 		}
-		
-		
+		b[i].num ++;		
 		for(int j=i+1; j<n; j++){
 			if(b[i].cizu == b[j].cizu ){
+				//printf("****\n");
 				b[i].num++;
 				b[j].f = 1;//遇见重复此组，num加一并将后面那个f赋空1
 			}
 		}
 		
+		
 	}
-	int e = 0;
+
+	int j = 0;
 	for(int i=0; i<n; i++){
+		
 		if(b[i].f!=1){
-			d[i] = b[i];
-			e++;
+			d[j] = b[i];
+			j++;
+			
+			//printf("%d\n",e);
+			
 		}
 	} 
 	//printf("%d\n",e);
 	
-	/*for(int i=0; i<e; i++){
-		printf("^^^^^^^^%s\n",d[i].cizu.c_str());
-	}*/
+	for(int i=0; i<j; i++){
+		printf("%s ",d[i].cizu.c_str());
+		printf("%d\n",d[i].num);
+	}
 	
 	return *d;
 	
@@ -88,13 +92,13 @@ struct ph phrase(struct str *a,int m){
 } 
 
 /*int main(){
-	str a[] = {{"Monday",0,1,0,false},{"Tuesday",0,1,0,false},{"Wednesday",0,1,0,false},{"Thursday",0,1,0,true},{"Friday",0,1,0,true}};
+	str a[] = {{"Monday",0,0,0,true},{"Tuesday",0,0,0,true},{"Wednesday",0,1,0,true},{"Monday",0,0,0,true},{"Tuesday",0,1,0,true},{"Wednesday",0,0,0,true},{"Monday",0,0,0,false},{"Tuesday",0,0,0,false},{"Wednesday",0,1,0,false},{"Thursday",0,0,0,true},{"Friday",0,0,0,true}};
 	int size = sizeof(a);
 	//
-	struct ph d = phrase(a,3,size);
+	struct ph d = phrase(a,3,11);
 
 	
-	//Monday Tuesday Wednesday Thursday
+//Monday Tuesday Wednesday Thursday
 //name出现次数//0为非单词 //判断是否遍历过//是否在文末 
 //Friday
 }*/
